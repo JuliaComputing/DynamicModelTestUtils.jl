d62c1ea107e0235dc8e5dbda0ff687670482a19b
Ben Chung <ckfinite@gmail.com>
2024-05-17 11:59:04 -0700

    Better `test_instantaneous` compatibility for both MTKv8 and MTKv9, as well as compliance with SII

--- src/test/instantaneous/instant.jl
+++ src/test/instantaneous/instant.jl
@@ -1,3 +1,4 @@
+
 function test_instantaneous(
     sys::ModelingToolkit.AbstractSystem,
     ic,
@@ -6,8 +7,8 @@ function test_instantaneous(
     if !SymbolicIndexingInterface.is_observed(checks)
         @assert false "The given check values are not observed values of the given system"
     end
-    sv = ModelingToolkit.varmap_to_vars(ic, states(sys); defaults=ModelingToolkit.defaults(sys))
-    pv = ModelingToolkit.varmap_to_vars(ic, parameters(sys); defaults=ModelingToolkit.defaults(sys))
+    sv = ModelingToolkit.varmap_to_vars(ic, variable_symbols(sys); defaults=default_values(sys))
+    pv = ModelingToolkit.varmap_to_vars(ic, parameter_symbols(sys); defaults=default_values(sys))
 
     obsfun = SymbolicIndexingInterface.observed(sys, checks)
     if SymbolicIndexingInterface.istimedependent(sys)
@@ -25,21 +26,9 @@ function test_instantaneous(
     ic,
     checks::Array;
     t = nothing) # todo: after porting to v9 use InitializationSystem to solve the checks system
-    if !all(SymbolicIndexingInterface.is_observed.((sys, ), checks))
-        @assert false "The given check values are not observed values of the given system"
-    end
-    sv = ModelingToolkit.varmap_to_vars(ic, states(sys); defaults=ModelingToolkit.defaults(sys))
-    pv = ModelingToolkit.varmap_to_vars(ic, parameters(sys); defaults=ModelingToolkit.defaults(sys))
-
-    obsfuns = SymbolicIndexingInterface.observed.((ODEFunction(sys),), checks)
-    if SymbolicIndexingInterface.is_time_dependent(sys)
-        @assert !isnothing(t) "The kwarg t must be given (and be a non-nothing value) if the system is time dependent"
-        return map(fun -> fun(sv, pv, t), obsfuns)
-    elseif !SymbolicIndexingInterface.constant_structure(sys)
-        @assert false "The system's structure must be constant to use test_instantaneous; to be fixed" # TODO
-    else
-        return map(fun -> fun(sv, pv), obsfuns)
-    end
+    prob = ODEProblem(sys, ic, (0.0, 0.0))
+    getter = getu(prob, checks)
+    return getter(prob)
 end
 
 export test_instantaneous
