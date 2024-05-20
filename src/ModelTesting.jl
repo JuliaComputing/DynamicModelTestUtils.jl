fe874a583ef80c5a76a072e7bcf7b1b47e4e7982
Ben Chung <ckfinite@gmail.com>
2024-05-20 11:16:20 -0700

    Clean up the older methods

--- src/ModelTesting.jl
+++ src/ModelTesting.jl
@@ -4,11 +4,8 @@ using ModelingToolkit, SciMLBase, SymbolicIndexingInterface
 abstract type DiscreteEvaluation end
 abstract type Metric end
 include("test/measured.jl")
-include("test/continuous/delta_sol.jl")
-include("test/discrete/single_shooting.jl")
-include("test/discrete/merge.jl")
-include("test/metric/metric.jl")
-include("test/discrete/compare.jl")
-include("test/instantaneous/instant.jl")
+include("test/discretize.jl")
+include("test/instant.jl")
+include("test/compare.jl")
 include("problem_config/problem.jl")
 end
