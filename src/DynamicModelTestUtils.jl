e577f2c37c97d208283a234650fb84c65b91e7f1
Ben Chung <ckfinite@gmail.com>
2024-06-21 22:08:36 -0700

    Regression testing integration method

--- src/DynamicModelTestUtils.jl
+++ src/DynamicModelTestUtils.jl
@@ -1,6 +1,7 @@
 module DynamicModelTestUtils
 using DataFrames, StatsBase, LinearAlgebra
 using ModelingToolkit, SciMLBase, SymbolicIndexingInterface
+using CSV, Test
 abstract type DiscreteEvaluation end
 abstract type Metric end
 include("test/measured.jl")
@@ -10,4 +11,5 @@ include("test/compare.jl")
 include("problem_config/problem.jl")
 include("deploy/deploy.jl")
 include("deploy/restore.jl")
+include("integration/regression.jl")
 end
--- test/deployment.jl
+++ test/deployment.jl
@@ -58,7 +58,6 @@ using Git, CSV
                 full_repo_path = joinpath(pwd(), "repo")
                 mkdir("data")
                 CSV.write("data/output.csv", d1)
-                println(readdir())
                 DynamicModelTestUtils.deploy(
                     pwd(), "data",
                     deploy_type = DynamicModelTestUtils.FilesystemDeployConfig(full_repo_path, "."),
@@ -69,6 +68,56 @@ using Git, CSV
                     restored = CSV.read("output.csv", DataFrame)
                     @test sum(compare(sol, restored)[:, :L∞]) < 0.01
                 end
+            end
+        end
+    end
+    @testset "RC regression" begin 
+        R = 1.0
+        C = 1.0
+        V = 1.0
+        @variables t
+        @named resistor = Resistor(R = R)
+        @named capacitor = Capacitor(C = C)
+        @named source = Voltage()
+        @named constant = Constant(k = V)
+        @named ground = Ground()
+    
+        @named sensor = PotentialSensor()
+        @named key_parameter = MeasureComponent()
+    
+        rc_eqs = [connect(constant.output, source.V)
+                connect(source.p, resistor.p)
+                connect(resistor.n, capacitor.p)
+                connect(capacitor.n, source.n, ground.g)
+                connect(sensor.p, capacitor.p)
+                sensor.phi ~ key_parameter.value]
+    
+        @named rc_model = ODESystem(rc_eqs, t,
+            systems = [resistor, capacitor, constant, source, ground, sensor, key_parameter])
+        sys = structural_simplify(rc_model)
+        prob1 = ODEProblem(sys, Pair[], (0, 10.0))
+        sol = solve(prob1, Tsit5())
+        prob2 = ODEProblem(sys, Pair[capacitor.C => 0.5], (0, 10.0))
+        sol2 = solve(prob2, Tsit5())
+        
+        println("\n\n\nstart deploy test")
+        mktempdir() do dir
+            cd(dir) do 
+                mkdir("repo")
+                run(`$(git()) -C repo init -q --bare`)
+                full_repo_path = joinpath(pwd(), "repo")
+                DynamicModelTestUtils.regression_test(Dict(:rc => sol), full_repo_path, "data", true; 
+                    deploy_only = true,
+                    make_output_dir = true)
+                println("deployed! to $full_repo_path")
+                run(`$(git()) clone -q -b data $(full_repo_path) worktree`)
+                println(readdir("worktree/"))
+                DynamicModelTestUtils.regression_test(Dict(
+                    :rc => sol
+                ), full_repo_path)
+                #DynamicModelTestUtils.regression_test(Dict(
+                #    :rc => sol2
+                #), full_repo_path)
             end
         end
     end
